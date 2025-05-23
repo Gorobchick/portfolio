915313837f36a92c9506d0843c2c658eeb5d758a
NullVoid <nullvoidhere@gmail.com>
2025-05-23 13:24:41 +0300

    main

---
+++ src/VideoCard.jsx
@@ -0,0 +1,18 @@
+
+function VideoCard(props) {
+    const {title} = props;
+    const {channelName} = props;
+    const {img} = props;
+  return (
+   <div className="video">
+          <img className="video-img" src={img} alt="React logo" />
+          <p>{title}</p>
+          <p>{channelName}</p>
+          <div className="video-footer">
+            <p>Вподобайки: 0</p>
+            <button>Вподобати</button>
+          </div>
+        </div>
+  );
+}
+export default VideoCard;
