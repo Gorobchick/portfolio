457ce10c5aff7fd6e0e53a53b9cb573cad1c5858
NullVoid <nullvoidhere@gmail.com>
2025-05-23 23:01:13 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import reactLogo from './assets/react.svg'
+
 import './App.css'
-import VideoCard from './VideoCard.jsx'
+import VideoCard from './Video/VideoCard.jsx'
+import { VIDEOS } from './videos.js'
 
 function App() {
   return (
     <>
       <div className="video-container">
-        <VideoCard title="Відео 1" channelName = "Factorio for kids" img={reactLogo}/>
+        {
+          VIDEOS.map((video) => {
+            return (
+              <VideoCard key={video.id} title={video.title} channelName={video.channelName} img={video.img}/>
+            )
+          })
+        }
       </div>
     </>
 
--- src/Video/VideoCard.jsx
+++ src/Video/VideoCard.jsx
@@ -1,16 +1,25 @@
+import'./Video.css'
+import { useState } from 'react';
+
 
 function VideoCard(props) {
     const {title} = props;
     const {channelName} = props;
     const {img} = props;
+    const [likeCount, setLikeCount] = useState(0);
+    const increaseLikeByOne = () => {
+        setLikeCount(likeCount + 1);
+        
+        
+    };
   return (
    <div className="video">
           <img className="video-img" src={img} alt="React logo" />
           <p>{title}</p>
           <p>{channelName}</p>
           <div className="video-footer">
-            <p>Вподобайки: 0</p>
-            <button>Вподобати</button>
+            <p>Вподобайки: {likeCount}</p>
+            <button onClick={increaseLikeByOne}>Вподобати</button>
           </div>
         </div>
   );
